52393b689b8e8abb9305a854a4a875f6a9515bb4
MingyeomKim <67851124+MingyeomKim@users.noreply.github.com>
2022-02-07 04:11:09 +0900

    [failure] 1764 듣보잡
    set으로 해보려다가 실패..
    그냥 한 배열에 몽땅 넣고 이중for문으로 해보겠습니다.

--- 1764.cpp
+++ 1764.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <string>
-#include <algorithm>
-#include <vector>
+#include <set>
 using namespace std;
 #define endl "\n";
 
@@ -10,31 +9,19 @@ int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-
-	int n; cin >> n; 
-	int m; cin >> m;
+	
 	int cnt = 0;
 
-	string* noHeard = new string[n];
-	string* noSeen = new string[n];
-	string* ans = new string[500000];
-
-	for (int i = 0; i < n; i++) {
-		cin >> noHeard[i];
-	}
-	int idx = 0;
-	for (int i = 0; i < m; i++) {
+	int n; int m; cin >> n >> m;
+	set<string> input;
+	for (int i = 0; i < n + m; i++) {
 		string temp; cin >> temp;
-
-		string *check = find(noHeard, noHeard + n, temp);
-		if (check != noHeard + n) {
-			ans[idx] = temp;
-			cnt++; idx++;
-		}
+		input.insert(temp);
 	}
+	cnt = input.size();
 
 	cout << cnt << endl;
-	for (int i = 0; i < cnt; i++) {
-		cout << ans[i] << endl;
+	for (string loop : input) {
+		cout << loop << endl;
 	}
 }
