1b78af8e26e1920f4eeea77527a6ac8b6b1d9579
MingyeomKim <67851124+MingyeomKim@users.noreply.github.com>
2022-02-07 04:27:45 +0900

    [succeed]1764 듣보잡

--- 1764.cpp
+++ 1764.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <set>
+#include <utility>
 using namespace std;
 #define endl "\n";
 
@@ -9,19 +10,24 @@ int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	
+
 	int cnt = 0;
 
 	int n; int m; cin >> n >> m;
 	set<string> input;
+	set<string> ans;
+
 	for (int i = 0; i < n + m; i++) {
 		string temp; cin >> temp;
-		input.insert(temp);
+		auto pair = input.insert(temp);
+		if (pair.second == false) {
+			ans.insert(*(pair.first));
+			cnt++;
+		}
 	}
-	cnt = input.size();
 
 	cout << cnt << endl;
-	for (string loop : input) {
+	for (auto loop : ans) {
 		cout << loop << endl;
 	}
 }
